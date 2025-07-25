b4bcdb6a899eac03e574ec72d1dab7ce81473062
samrin <samreenshamaa@gmail.com>
2025-07-25 17:32:30 +0530

    terraform

---
+++ 90-components/variables.tf
@@ -0,0 +1,17 @@
+variable "components" {
+    default = {
+        
+        cart = {
+            rule_priority = 30
+        }
+        shipping = {
+            rule_priority = 40
+        }
+        payment = {
+            rule_priority = 50
+        }
+        frontend = {
+            rule_priority = 10
+        }
+    }
+}
