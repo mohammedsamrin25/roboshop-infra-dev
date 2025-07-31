4933949cc1355b530e6be5ed57754ec9197bf8cc
samrin <samreenshamaa@gmail.com>
2025-07-31 13:15:58 +0530

    terraform

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -9,7 +9,8 @@
     volume_size = 50
     volume_type = "gp3" # or "gp2", depending on your preference
   }
-
+  user_data = file("bastion.sh")
+  iam_instance_profile = "TerraformAdmin"
   tags = merge(
     local.common_tags,
     {
